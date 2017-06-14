86748a39929bc3777ef2a3a19a03bfe773d71c03
Dustin Liu <liu252@mail.chapman.edu>
2017-06-14 17:56:33 +0800

    Unity 4.6

---
+++ NumberWizardUI/Assets/NumberWizard.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour 
+{
+	
+	int max;
+	int min;
+	int guess;
+	
+	public Text text;
+	
+	public int maxGuessesAllowed = 10;
+	// Use this for initialization
+	void Start ()
+	{
+		StartGame();
+	}
+	
+	void StartGame ()
+	{
+		max = 1000;
+		min = 1;
+		max = max + 1;
+		NextGuess();
+	}
+	
+	
+	void NextGuess()
+	{
+		guess = Random.Range(min,max);
+		text.text = guess.ToString();
+		maxGuessesAllowed--;
+		if(maxGuessesAllowed <= 0)
+		{
+			Application.LoadLevel("Win");
+		}
+	}
+
+	
+	public void GuessHigher()
+	{
+		min = guess;
+		NextGuess();
+	}
+	
+	public void GuessLower()
+	{
+		max = guess;
+		NextGuess ();
+	}
+}
